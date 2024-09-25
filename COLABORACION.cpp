561e1be042a3014f76f27a980e03d08601c962cf
Ana20Pao <ana.paola.gl20@outlook.com>
2024-09-24 21:54:38 -0600

    Update COLABORACION.cpp

--- COLABORACION.cpp
+++ COLABORACION.cpp
@@ -6,5 +6,9 @@ int main()
     int hora, minuto, minutoNuevo;
     printf("Hora: ");
     scanf_s("%i", &hora); 
+    printf("Minutos:");
+    scanf_s("%i", &minuto);
+    printf("Añadir:");
+    scanf_s("%i", &minutoNuevo); 
 }
 
