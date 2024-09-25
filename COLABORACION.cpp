5e533175f3397cfce0b929f9c71868d147a74e74
StowDet <stowdet@gmail.com>
2024-09-25 00:03:03 -0600

    Cambio en hora y resultado

--- COLABORACION.cpp
+++ COLABORACION.cpp
@@ -8,16 +8,19 @@ int main()
     scanf_s("%i", &hora); 
     printf("Minutos:");
     scanf_s("%i", &minuto);
-    printf("Añadir:");
+    printf("Agregar:");
     scanf_s("%i", &minutoNuevo); 
     minuto = minuto + minutoNuevo;
     if (minuto >= 60)
     {
         hora = hora+minuto/60;
         minuto = minuto%60;
+        if (hora >=24)
+    	 hora = (hora / 24) + (hota % 24);
+    
     }
-    if (hora >=24)
-    { hora = 0;
-    }
+    printf("La nueva hora es: %2.2i:%2.2i", hora, minuto);
+   
+    
 }
 
